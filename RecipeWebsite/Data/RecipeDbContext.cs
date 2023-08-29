f1a99ab6c4e5ca291d5cff815c40ea2e12706710
Kenneth LaVoie <kenlavoie19@gmail.com>
2023-08-29 19:00:07 -0400

    Added Files for DbContext and Added Views for Recipes.

---
+++ RecipeWebsite/Data/RecipeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using RecipeWebsite.Models.Domain;
+
+namespace RecipeWebsite.Data
+{
+    public class RecipeDbContext : DbContext
+    {
+        public RecipeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+            public DbSet<Recipe> Recipe { set; get; }
+        
+    }
+}
---
+++ RecipeWebsite/Models/RecipeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeWebsite.Models
+{
+    public class RecipeViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
